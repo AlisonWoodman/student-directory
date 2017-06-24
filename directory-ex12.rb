5e8aaa2b2db832b6804714d1c42424544c84d40a
Alison Woodman <aliwoodman@gmail.com>
2017-06-24 12:04:32 +0200

    ex12

---
+++ directory-ex12.rb
@@ -0,0 +1,49 @@
+#12. What happens if the user doesn't enter any students? It will try to print
+# an empty list. How can you use an if statement (Control Flow) to only print the
+# list if there is at least one student in there?
+
+def input_students
+  students = []
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  name = gets.chomp
+
+  while !name.empty? do
+  students << {name: name, cohort: "november"}
+  puts "Now we have #{students.count} students"
+  name = gets.chomp
+  end
+  return students
+end
+
+def print_header
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+end
+
+def print_footer(students)
+  unless students.count == 1
+  puts "Overall, we have #{students.count} great students"
+else
+  puts "Overall, we have #{students.count} great student"
+end
+end
+
+def printing(students)
+if students.count >= 1
+ print_header
+ print(students)
+ print_footer(students)
+ else
+   puts "There are no students :("
+ end
+end
+
+students = input_students
+printing(students)
