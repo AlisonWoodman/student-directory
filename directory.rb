1e3fd41f22213bd07cdb3fc3db7bddf834235431
Alison Woodman <aliwoodman@gmail.com>
2017-06-25 08:43:10 +0200

    Step 9: add interactive menu

--- directory.rb
+++ directory.rb
@@ -14,42 +14,85 @@ students = [
 ]
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the name of the student"
   # create an empty array
   students = []
   # get the first name
   name = gets.chomp
+  puts "Please enter the cohort of the student"
+  cohort = gets.chomp
+    if cohort == ""
+      cohort = "default"
+    else
+    end
   # while the name is not empty, repeat this code
-  while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    # get another name from the user
-    name = gets.chomp
-  end
-  # return the array of students
-  students
+    while !name.empty? do
+      # add the student hash to the array
+      students << {name: name, cohort: cohort}
+      if students.count > 1
+        puts "Now we have #{students.count} students"
+      else
+        puts "Now we have #{students.count} student"
+      end
+      # get another name from the user
+      puts "Please enter the name of the student"
+      puts "(or hit return to finish)"
+      name = gets.chomp
+        unless name.empty?
+        puts "Please enter the cohort of the student"
+        cohort = gets.chomp
+          if cohort == ""
+            cohort = "default"
+          else
+          end
+        end
+    end
+      # return the array of students
+  return students
 end
 
-
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "\n-------------"
+  puts "The students of Villains Academy:"
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.sort_by! {|student| student[:cohort]}
+  students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 def print_footer(names)
-  puts "Overall, we  have #{names.count} great students"
+  if names.count > 1
+    puts "Overall, we  have #{names.count} great students"
+  else
+    puts "Overall, we  have #{names.count} great student"
+  end
+    puts "-------------"
+    puts "\n"
 end
 
-# nothing happens until we call the methods
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+interactive_menu
